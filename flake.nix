9202b133845d41261cfa08a7771465161c907b2e
4jamesccraven <4jamesccraven@gmail.com>
2026-09-16 07:00:08 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "";
+
+  inputs.nixpkgs.url = "https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      eachDefaultSystem =
+        function:
+        lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachDefaultSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python314
+          ];
+        };
+      });
+    };
+}
---
+++ sglayout.py
@@ -0,0 +1,71 @@
+import argparse
+from typing import cast
+from enum import Enum
+from itertools import repeat
+
+class BlockState(Enum):
+    SOIL = 1
+    WATER = 2
+    UNUSABLE = 3
+    IGNORE = 4
+
+    def display(self) -> str:
+        match self:
+            case BlockState.SOIL:
+                return '\x1b[92m█\x1b[0m' #]]
+            case BlockState.WATER:
+                return '\x1b[94m░\x1b[0m' #]]
+            case BlockState.UNUSABLE:
+                return '\x1b[93m▓\x1b[0m' #]]
+            case BlockState.IGNORE:
+                return ' '
+
+def cli() -> int:
+    p = argparse.ArgumentParser('test')
+    p.add_argument('w', type=int)
+    w = cast(int, p.parse_args().w)
+    return w
+
+def init_grid(width: int) -> list[list[BlockState]]:
+    grid = []
+    for _ in repeat(None, width):
+        grid.append([BlockState.SOIL] * width)
+    return grid
+
+def apply_pattern(congruency: int, grid: list[list[BlockState]]) -> None:
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if ((2*x + y) % 5) == congruency:
+                grid[y][x] = BlockState.WATER
+
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] == BlockState.WATER:
+                continue
+
+            surroundings: list[BlockState] = []
+            if x + 1 < len(grid[0]):
+                surroundings.append(grid[y][x+1])
+            if x - 1 >= 0:
+                surroundings.append(grid[y][x-1])
+            if y + 1 < len(grid):
+                surroundings.append(grid[y+1][x])
+            if y - 1 >= 0:
+                surroundings.append(grid[y-1][x])
+
+            if not any(block == BlockState.WATER for block in surroundings):
+                grid[y][x] = BlockState.UNUSABLE
+
+def main() -> None:
+    grid = init_grid(cli())
+
+    apply_pattern(0, grid)
+
+    for row in grid:
+        for block in row:
+            print(f'{block.display()}', end='')
+        print('\n', end='')
+
+
+if __name__ == '__main__':
+    main()
