a3fe00c8ad2036e0d1204677604a9e6ad364366e
4jamesccraven <4jamesccraven@gmail.com>
2026-09-19 11:25:43 -0400

    feat: add functionality to calculate the optimal layout

--- src/SugarCane/Layout.hs
+++ src/SugarCane/Layout.hs
@@ -1,6 +1,6 @@
 module SugarCane.Layout where
 
-import Data.List (intercalate)
+import Data.List (findIndices, intercalate)
 import SugarCane.Farm qualified as Farm
 import SugarCane.Geometry (deindex, indexGrid, neighbours)
 
@@ -38,7 +38,7 @@ mutableBlock b = b /= Blocked
 phase :: Int -> Int -> Int
 phase x z = (2 * x + z) `mod` 5
 
--- | Applies an optimal sugarcane pattern to a farm. Does not gurantee optimality.
+-- | Applies an optimal sugarcane pattern to a farm. Does not guarantee optimality.
 applyLayout :: Int -> Layout -> Layout
 applyLayout offset Layout {width = w, height = h, grid = blocks} =
   let withIndices = indexGrid blocks
@@ -67,6 +67,40 @@ applyLayout offset Layout {width = w, height = h, grid = blocks} =
    in Layout {width = w, height = h, grid = deindex withIrrigation}
 
 -----------------------------------------------------------
+-- Optimality
+-----------------------------------------------------------
+
+-- | Determine how optimal a layout is by counting all `Irrigated` blocks.
+optimalityScore :: Layout -> Int
+optimalityScore lay = optimalityScore' Irrigated lay
+
+-- | Determine how optimal a layout is based on the count of an arbitrary
+-- | `LayoutBlock` state.
+optimalityScore' :: LayoutBlock -> Layout -> Int
+optimalityScore' state lay = length $ filter (\s -> s == state) (concat $ grid lay)
+
+-- | Finds the best offsets by optimising for the most possible irrigated blocks.
+optimalOffsets :: Farm.Farm -> [Int]
+optimalOffsets f =
+  let allLayouts = layouts f
+      allScores = map optimalityScore allLayouts
+      maxScore = maximum allScores
+   in findIndices (\s -> s == maxScore) allScores
+
+-- | Find all possible layouts for the farm.
+layouts :: Farm.Farm -> [Layout]
+layouts f =
+  let farm = liftFarm f
+   in map (\c -> applyLayout c farm) [0 .. 4]
+
+-- | Finds the optimal `Layout`s for this particular farm.
+optimalLayout :: Farm.Farm -> [Layout]
+optimalLayout f =
+  let farm = liftFarm f
+      offsets = optimalOffsets f
+   in map (\c -> applyLayout c farm) offsets
+
+-----------------------------------------------------------
 -- Conversions for `Farm` -> `Layout`
 -----------------------------------------------------------
 
@@ -76,7 +110,7 @@ liftFarmBlock fb = case fb of
   Farm.Available -> Unirrigated
   Farm.Blocked -> Blocked
 
--- | Lifts an entire `Farm` to a `Layout`
+-- | Lifts an entire `Farm` to a `Layout`. This conversion is unoptimised.
 liftFarm :: Farm.Farm -> Layout
 liftFarm Farm.Farm {width = w, height = h, blocks = grid} =
   Layout
